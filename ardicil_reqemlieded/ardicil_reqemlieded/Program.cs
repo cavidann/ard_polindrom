166a463b82226665e46283270a8c41c440972e57
cavidann <cavi98@icloud.com>
2017-05-08 17:56:35 -1100

    ATM

---
+++ ardicil_reqemlieded/ardicil_reqemlieded/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ardicil_reqemlieded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baslangic = 10;
+            int son = 50;
+            for (int i=baslangic; i<son; i++)
+            {
+
+                int j = i % 10;
+                int j1 = i/ 10;
+                int j2;
+                j2=j1 + 1;
+                if (j == j2)
+                {
+                    Console.WriteLine(i);
+                    
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ polindrom/polindrom/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("baslangi ededi daxil edin: ");
+            int baslangic = Convert.ToInt16(Console.ReadLine());
+            Console.Write("baslangi ededi daxil edin: ");
+            int son= Convert.ToInt16(Console.ReadLine());
+            for (int i = baslangic; i < son; i++)
+            {
+                int ters = 0;
+                int son_reqem = 0;
+                int x = i;
+                
+                while (x > 0)
+                {
+                    son_reqem = x % 10;
+                    x /= 10;
+                    ters = ters * 10 + son_reqem;            
+                }
+                if (i == ters&&ters>10)
+                {
+                    Console.WriteLine(ters);
+                }                   
+            }            
+            Console.ReadLine();
+        }
+    }
+}
